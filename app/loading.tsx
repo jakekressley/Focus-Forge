0b0d14efca5008169f7007e31bf265fd36744c2b
jakekressley <kressleyjake@gmail.com>
2024-02-07 01:34:27 -0500

    updated tree page and loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
     return (
-        <div>
-            <h1>Loading...</h1>
-        </div>
+        <div className="flex justify-center items-center h-100 p-8">
+        <h1 className="--header-name font-bold text-3xl mt-40 p-8">Loading...</h1>
+      </div>
     )
 }
--- app/tree/page.tsx
+++ app/tree/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const SkillTree = () => {
   return (
-    <div>
-      <h1>The skill tree will go here</h1>
+    <div className="flex justify-center items-center h-100 p-8">
+      <h1 className="--header-name font-bold text-8xl mt-40 p-8">Skill Tree Coming Soon!</h1>
     </div>
   )
 }
