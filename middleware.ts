a34588e193a55f7a0d899ed6df251ae30af6eb18
jakekressley <kressleyjake@gmail.com>
2024-02-22 10:38:13 -0500

    reverted and connected mongo intergration in deployment

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export async function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
 
     // define paths that are public (accessible without authentication)
-    const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail' || path ==='/study' || path === '/about' || path === '/tips'
+    const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail'
 
     const token = await request.cookies.get('token')?.value || ''
 
@@ -25,7 +25,7 @@ export async function middleware(request: NextRequest) {
     // wait for cookies request
     // redirect logic based on path and if they have token
     if (isPublicPath && token) {
-        return NextResponse.redirect(new URL('/study', request.nextUrl))
+        return NextResponse.redirect(new URL('/', request.nextUrl))
     }
 
     // I think the problem is that it is doing this too fast
@@ -37,11 +37,11 @@ export async function middleware(request: NextRequest) {
 export const config = {
     matcher: [
         '/',
-        //'/tips',
-        //'/about',
+        '/tips',
+        '/about',
         '/login',
         '/signup',
         '/verifyemail',
-        //'/study',
+        '/study',
     ]
 }
