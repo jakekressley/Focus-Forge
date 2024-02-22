9419fc447bdd4f671f08908b2de8f050d9d42a17
jakekressley <kressleyjake@gmail.com>
2024-02-22 10:50:48 -0500

    more changs

--- middleware.ts
+++ middleware.ts
@@ -4,44 +4,26 @@ import type { NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
 
-    // define paths that are public (accessible without authentication)
-    const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail' || path === '/study'
+    // Define paths that are public (accessible without authentication)
+    const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail' || path === '/study';
 
-    const token = await request.cookies.get('token')?.value || ''
+    const token = await request.cookies.get('token')?.value || '';
 
-    
-    const response = NextResponse.json({
-        message: "Login successful",
-        success:true,
-        token: token,
-    })
-    
-
-    
-    response.cookies.set("token", token, {
-        httpOnly: true,
-    })
-    
-    // wait for cookies request
-    // redirect logic based on path and if they have token
-    if (isPublicPath && token) {
-        return NextResponse.redirect(new URL('/study', request.nextUrl))
-    }
-
-    // I think the problem is that it is doing this too fast
     if (!isPublicPath && !token) {
-        return NextResponse.redirect(new URL('/login', request.nextUrl))
+        // Redirect to the login page if trying to access a protected route without a token
+        return NextResponse.redirect(new URL('/login', request.nextUrl));
     }
+
+    // Continue to the next middleware or handler
+    return NextResponse.next();
 }
 
 export const config = {
+    // Define the paths that should be protected by the middleware
     matcher: [
         '/',
-        //'/tips',
-        //'/about',
-        //'/login',
-        '/signup',
-        '/verifyemail',
+        '/tips',
+        '/about',
         '/study',
     ]
-}
+};
